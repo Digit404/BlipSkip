a0ff2367a31ce1dad2a231584c9e0f92606de374
Digit <digit.10k@gmail.com>
2024-01-24 18:09:20 -0800

    screw it, firefox is getting the same manifest version as chrome

--- CreatePackage.ps1
+++ CreatePackage.ps1
@@ -1,23 +1,7 @@
-param (
-    [switch]$Firefox
+$files = (
+    ".\content-script.js",
+    ".\manifest.json",
+    ".\icons\"
 )
 
-$script = ".\content-script.js"
-$manifest = ".\manifest.json"
-$iconsFolder = ".\icons\"
-
-if ($Firefox) {
-    Copy-Item -Path ".\manifest-firefox.json" -Destination $manifest
-    $packageName = ".\package-firefox.zip"
-} else {
-    Copy-Item -Path ".\manifest-chrome.json" -Destination $manifest
-    $packageName = ".\package-chrome.zip"
-}
-
-$files = @($script, $manifest, $iconsFolder)
-
-$command = "7z a -tzip " + $packageName + " " + $files
-
-Write-Host $command
-
-Invoke-Expression $command
+Compress-Archive -Path $files -DestinationPath "AdSpeedup.zip"
